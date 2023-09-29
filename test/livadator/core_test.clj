799131f6930bebeec710435f96ec2e27791e85e9
Roman Makhlin <roman.v.makhlin@gmail.com>
2023-09-29 17:19:19 +0200

    what is the ... nevermind

--- test/livadator/core_test.clj
+++ test/livadator/core_test.clj
@@ -110,34 +110,3 @@
     (testing "If no multiple? key might be either way"
       (is (= {} (validate {:key [0 1 2 3]} {:key {:validators int?}})))
       (is (= {} (validate {:key 0} {:key {:validators int?}}))))))
-
-(deftest complex-test
-  (let [stop-on-first-error? true
-        ignore-not-in-schema? true]
-    (pprint (validate {:a-1  1
-                       :a-2  ""
-                       :a-3  []
-                       :a-6  [0 1 2 3]
-                       :a-7  [:a :b :c "a" "b" "c"]
-                       :a-8  {:a-8-1 1
-                              :a-8-2 "2"
-                              :a-8-3 [{:a-8-3-1 true}]}
-                       :a-9  []
-                       :a-10 0}
-                      {:a-1  int?
-                       :a-2  [string?]
-                       :a-3  {:multiple? true :validators int?}
-                       :a-4  {:required? false :validators int?}
-                       :a-5  {:required? true :validators string?}
-                       :a-6  [int? (partial < -1)]
-                       :a-7  {:multiple?  true
-                              :validators (fn [val] (not (= :b (keyword val))))}
-                       :a-8  {:validators {:a-8-1 int?
-                                           :a-8-2 string?
-                                           :a-8-3 {:multiple?  true
-                                                   :validators {:a-8-3-1 boolean?
-                                                                :a-8-3-2 {:required?  true
-                                                                          :validators int?}}}}}
-                       :a-9  {:multiple? false :validators int?}
-                       :a-10 {:multiple? true :validators int?}}
-                      (Options. (dont stop-on-first-error?) (dont ignore-not-in-schema?))))))
